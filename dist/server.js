3d374660a47ee6ee2e7e9fc3a425d5313e33a25f
Juan Sebastian Nieto Solano <108822850+jniet11@users.noreply.github.com>
2025-02-22 16:15:35 -0500

    se agregan validaciones y conexión con la base de datos

--- dist/server.js
+++
@@ -1,8 +0,0 @@
-// src/server.ts
-import app from './app.js';
-import dotenv from 'dotenv';
-dotenv.config();
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`🚀 Servidor ejecutándose en http://localhost:${PORT}`);
-});
